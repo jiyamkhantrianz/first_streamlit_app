db4189ab83bc0507d76b0c515192cdba158a1142
jiyamkhantrianz <123239543+jiyamkhantrianz@users.noreply.github.com>
2023-01-25 17:34:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,24 +43,25 @@ try:
 except URLError as e:
     streamlit.error()
   
-streamlit.text(fruityvice_response)
-streamlit.write('The user entered ', fruit_choice) 
+
+streamlit.header("Fruityvice Fruit Advice!")
+#snowflake related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_row = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 
 
-
-#import streamlit
-# Don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains : " )
-streamlit.dataframe(my_data_rows)
-my_data_row = my_cur.fetchone()
+
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
